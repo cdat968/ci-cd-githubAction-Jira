02251c656a4107d16c66aebb7a3b08effdc06d16
NguyenChauQuocDat <cdat789@gmail.com>
2026-05-21 15:10:56 +0700

    Skip dotenv on CI environment

--- emp-kiosk-master/hr-web/jest.setup.ts
+++ emp-kiosk-master/hr-web/jest.setup.ts
@@ -1,3 +1,6 @@
-import dotenv from 'dotenv'
-import path from 'path'
-dotenv.config({ path: path.resolve(__dirname, '../.env') })
+import dotenv from "dotenv";
+import path from "path";
+// dotenv.config({ path: path.resolve(__dirname, '../.env') })
+if (!process.env.CI) {
+    dotenv.config({ path: path.resolve(__dirname, "../.env") });
+}
